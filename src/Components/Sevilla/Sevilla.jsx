ebf76dcc2b3301a79a979789be7e3cfc1d661d86
Nele <nele@zitau.de>
2026-06-12 08:49:49 +0200

    Picture slider

--- src/Components/Sevilla/Sevilla.jsx
+++ src/Components/Sevilla/Sevilla.jsx
@@ -1,29 +1,67 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './Sevilla.css'
 import sevilla_1 from '../../assets/sevilla_1.JPG'
 import sevilla_2 from '../../assets/sevilla_2.JPG'
 import sevilla_3 from '../../assets/sevilla_3.JPG'
+import next_icon from '../../assets/arrow.png'
+import back_icon from '../../assets/arrow.png'
 
 const Sevilla = () => {
+
+        const slider = useRef();
+        let tx = 0;
+
+    const slideForward = () => {
+        if(tx > -50){
+            tx -= 25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+
+    const slideBackward = () => {
+        if(tx > 0){
+            tx += 25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+
     return (
-        <div className='programs'>
-            <div className="program">
-                <img src={sevilla_2} alt=""/>
-                <div className="caption">
-                    <p>Plaza de España</p>
-                </div>
-            </div>
-            <div className='program'>
-                <img src={sevilla_1} alt=""/>
-                <div className="caption">
-                    <p>Itálica</p>
-                </div>
-            </div>
-            <div className='program'>
-                <img src={sevilla_3} alt=""/>
-                <div className="caption">
-                    <p>Plaza de América</p>
-                </div>
+        <div className='Sevilla'>
+            <img src={next_icon} alt="" className='next-btn' onClick={slideForward}/>
+            <img src={back_icon} alt="" className='back-btn' onClick={slideBackward}/>
+            <div className="slider">
+                <ul ref={slider}>
+                    <li>
+                        <div className="slide">
+                            <div className="program">
+                                <img src={sevilla_2} alt=""/>
+                                <div className="caption">
+                                    <p>Plaza de España</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className='program'>
+                                <img src={sevilla_1} alt=""/>
+                                <div className="caption">
+                                    <p>Itálica</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className='program'>
+                                <img src={sevilla_3} alt=""/>
+                                <div className="caption">
+                                    <p>Plaza de América</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     )
